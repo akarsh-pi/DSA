e6769c86db0648200c8c7449825e6d2ca4bda99e
akarsh-pi <akarshmishra166@gmail.com>
2026-01-30 23:46:44 +0530

    Day 8

---
+++ Recursion/Permutations.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Permutations {
+    public static String newstr = "";
+    public static void permutations(String str, String perm){
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            char currchar = str.charAt(i);
+            String newstr = str.substring(0, i) + str.substring(i+1);
+            permutations(newstr, perm+currchar);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        permutations(str, "");
+    }
+}
