7d86f8d6d2c64280c1e4840d8b3b7d8afe5e427a
akarsh-pi <akarshmishra166@gmail.com>
2026-02-06 19:31:51 +0530

    Day 15

--- Arrays EASY/Union.java
+++ Arrays EASY/Union.java
@@ -1,26 +1,17 @@
 import java.util.*;
 public class Union {
     public static void Union(int a[], int b[]){
-        int newarr[] = new int[a.length + b.length+2];
-        int x=0;
-        for(int i=0; i<a.length; i++){
-            for(int j=0; j<b.length; j++){
-                if(a[i] == b[j]){
-                    newarr[x] = a[i];
-                    x++;
-                }
-                else{
-                    newarr[x] = a[i];
-                    x++;
-                    newarr[x] = b[j];
-                    x++;
-                }
-            }
-        }
-        System.out.print("The union of two arrays are: ");
-        for(int i=0; i<newarr.length; i++){
-            System.out.print(newarr[i] + " ");
-        }
+         HashSet<Integer> set = new HashSet<>();
+         for(int i=0; i<a.length; i++){
+            set.add(a[i]);
+         }
+         for(int i=0; i<b.length; i++){
+            set.add(b[i]);
+         }
+         System.out.print("The union of the Arrays are:");
+         for(int i: set){
+            System.out.print( i +" ");
+         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
