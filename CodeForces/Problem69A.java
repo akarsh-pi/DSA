400dfa1ca94a03355e2aa6189420efde80717d95
akarsh-pi <akarshmishra166@gmail.com>
2026-02-12 20:24:55 +0530

    Day 19

--- CodeForces/Problem69A.java
+++ CodeForces/Problem69A.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class practice {
+public class Problem69A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
