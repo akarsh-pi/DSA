49b08942b369a6cc11120006864e70e34aea6609
akarsh-pi <akarshmishra166@gmail.com>
2026-03-03 22:42:31 +0530

    Day 32

--- CodeForces/Problem2182B.java
+++ CodeForces/Problem2182B.java
@@ -1,6 +1,5 @@
 import java.util.*;
-
-public class practice {
+public class Problem2182B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
